793977f1594d36ce5bc65655db09c3ff1cce9974
Mr-EmPee <manuel.palumbo@email.com>
2021-07-07 01:43:16 +0200

    An update can consists of a download link, verion and changelog

--- src/main/java/tk/empee/updateChecker/Update.java
+++ src/main/java/tk/empee/updateChecker/Update.java
@@ -1,4 +1,40 @@
 package tk.empee.updateChecker;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Update {
+
+    private final String version;
+
+    public Update(String version) {
+        this.version = version;
+    }
+    public String getVersion() {
+        return version;
+    }
+
+    private String downloadLink;
+    public String getDownloadLink() {
+        return downloadLink;
+    }
+    public void setDownloadLink(String downloadLink) {
+        this.downloadLink = downloadLink;
+    }
+
+    private final List<String> bugFixes = new ArrayList<>();
+    public void addBugFix(String bugFix) {
+        bugFixes.add(bugFix);
+    }
+    public List<String> getBugFixes() {
+        return bugFixes;
+    }
+
+    private final List<String> otherInfos = new ArrayList<>();
+    public void addOtherInfo(String info) {
+        otherInfos.add(info);
+    }
+    public List<String> getOtherInfos() {
+        return otherInfos;
+    }
 }
