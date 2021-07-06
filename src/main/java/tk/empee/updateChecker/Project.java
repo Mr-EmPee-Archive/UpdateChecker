a4f2a3511fa286b2ca0941c56e4fae73dd09c00d
Mr-EmPee <manuel.palumbo@email.com>
2021-07-07 01:45:03 +0200

    The project now it has basic attributes like version and name

--- src/main/java/tk/empee/updateChecker/Project.java
+++ src/main/java/tk/empee/updateChecker/Project.java
@@ -1,5 +1,35 @@
 package tk.empee.updateChecker;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+
 public class Project {
+
+    private final JarFile projectJar;
+    private final String currentVersion;
+
+    /**
+     * @throws IOException if an I/O error has occurred
+     * @throws SecurityException if access to the file is denied by the SecurityManager
+     */
+    public Project(File project) throws IOException {
+        projectJar = new JarFile(project);
+
+        Manifest projectManifest = projectJar.getManifest();
+        currentVersion = projectManifest.getMainAttributes().getValue("Implementation-Version");
+    }
+
+    public String getName() {
+        return projectJar.getName();
+    }
+    public String getCurrentVersion() {
+        return currentVersion;
+    }
+
+    public String toString() {
+        return getName() + " (" + getCurrentVersion() +  ")";
+    }
 
 }
