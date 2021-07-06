06f5ebd53f9d63b8ba090f1afd1539ec14862b89
Mr-EmPee <manuel.palumbo@email.com>
2021-07-07 01:41:14 +0200

    Conformed interface with newly added Update.java class

--- src/main/java/tk/empee/updateChecker/UpdateListener.java
+++ src/main/java/tk/empee/updateChecker/UpdateListener.java
@@ -2,7 +2,7 @@ package tk.empee.updateChecker;
 
 public interface UpdateListener {
 
-    void onOutdated(Project project);
+    void onOutdated(Project project, Update update);
     void onUpdated(Project project);
 
 }
