33ef23adfe15ed355ae1432c2ff118f58d8efbbb
Mr-EmPee <manuel.palumbo@email.com>
2021-07-19 15:31:43 +0200

    The project can now check if it is updated. If an error occurs during the initialization process the thrown exception will be more detailed.

--- src/main/java/tk/empee/updateChecker/Project.java
+++ src/main/java/tk/empee/updateChecker/Project.java
@@ -1,24 +1,64 @@
 package tk.empee.updateChecker;
 
+import tk.empee.updateChecker.utils.Version;
+
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.jar.JarFile;
 import java.util.jar.Manifest;
 
 public class Project {
 
     private final JarFile projectJar;
+
     private final String currentVersion;
+    private final String latestVersion;
+
+    private final URL changelogFolderURl;
+    private final String changelogNamingConvention;
+
+    private final URL latestJarDownloadURL;
 
     /**
-     * @throws IOException if an I/O error has occurred
-     * @throws SecurityException if access to the file is denied by the SecurityManager
+     * @throws IOException if it occurs a problem while parsing a manifest
+     * it will be thrown a general IOException. The cause can be retrieved using #getCause()
      */
     public Project(File project) throws IOException {
         projectJar = new JarFile(project);
 
-        Manifest projectManifest = projectJar.getManifest();
-        currentVersion = projectManifest.getMainAttributes().getValue("Implementation-Version");
+        Manifest latestManifest;
+        try {
+            Manifest projectManifest = projectJar.getManifest();
+            currentVersion = projectManifest.getMainAttributes().getValue("Specification-Version");
+            latestManifest = getLatestManifest(projectManifest.getMainAttributes().getValue("Latest-ManifestURL"));
+        } catch (Exception e) {
+            throw new IOException("Error while parsing the jar manifest", e);
+        }
+
+        try {
+
+            latestVersion = latestManifest.getMainAttributes().getValue("Specification-Version");
+            changelogFolderURl = new URL(latestManifest.getMainAttributes().getValue("Changelog-FolderURL"));
+            changelogNamingConvention = latestManifest.getMainAttributes().getValue("Changelog-NamingConvention");
+            latestJarDownloadURL = new URL(latestManifest.getMainAttributes().getValue("Jar-DownloadURL"));
+
+        } catch (Exception e) {
+            throw new IOException("Error while parsing the latest manifest", e);
+        }
+
+    }
+
+    private Manifest getLatestManifest(String urlString) throws MalformedURLException {
+
+        try {
+            URL latestManifestURL = new URL(urlString);
+            return new Manifest(latestManifestURL.openStream());
+        } catch (IOException e) {
+            throw new MalformedURLException("The latest manifest URL is malformed");
+        }
+
     }
 
     public String getName() {
@@ -26,6 +66,19 @@ public class Project {
     }
     public String getCurrentVersion() {
         return currentVersion;
+    }
+
+    public Update isOutdated() {
+
+        Update update = null;
+
+        if(!(Version.compare(currentVersion, latestVersion) < 0)) {
+            update = new Update(latestVersion, latestJarDownloadURL);
+            update.loadChangelogs(changelogFolderURl, changelogNamingConvention);
+        }
+
+        return update;
+
     }
 
     public String toString() {
